be10bcd6b6ce6c4548c1c7fe7942f06f4f9fa5a8
Wilmer Vazquez <vazquezwilmer@gmail.com>
2018-01-22 20:37:29 -0500

    Continue to link the site with gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
 'use strict';
 
 const gulp = require('gulp'),
-      sass = require('gulp-sass');
+      sass = require('gulp-sass')
+      del = require('del');
 
 
 gulp.task('compileSass', () => {
-  gulp.src('public/styles/app.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('css'));
+  return gulp.src('public/styles/app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', () =>{
+  gulp.watch('public/styles/**/*.scss', ['compileSass']);
+});
+
+gulp.task('build', ['compileSass'], () => {
+  return gulp.src(['css/app.css'], { base: './'})
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', () => {
+  del('dist');
+});
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
 });
