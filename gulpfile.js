dc0af287fce23f48aa9b6dd8a9739057507e4269
wvazquez <vazquezwilmer@gmail.com>
2018-01-23 22:48:13 -0500

    Adds Browser-sync live reload functionality.

--- gulpfile.js
+++ gulpfile.js
@@ -7,52 +7,69 @@ const gulp = require('gulp'),
       uglify = require('gulp-uglify'),
       rename = require('gulp-rename'),
       maps = require('gulp-sourcemaps'),
-      pug = require('gulp-pug');
+      pug = require('gulp-pug'),
+      browserSync = require('browser-sync').create(),
+      runSequence = require('run-sequence');
+
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('compileSass', () => {
+  return gulp.src('styles/app.scss')
+        .pipe(maps.init())
+        .pipe(sass())
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('compilePug', () => {
+  return gulp.src('views/index.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream());
+});
 
 gulp.task('concatScripts', () => {
   return gulp.src(['js/sticky.js'])
       .pipe(maps.init())
       .pipe(concat('app.js'))
       .pipe(maps.write('./'))
-      .pipe(gulp.dest('js'));
+      .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('minifyScripts', ['concatScripts'], () => {
   return gulp.src('js/app.js')
       .pipe(uglify())
       .pipe(rename('app.min.js'))
-      .pipe(gulp.dest('js'));
+      .pipe(gulp.dest('dist/js'));
+});
+gulp.task('watch::js', ['minifyScripts'], function (done) {
+    browserSync.reload();
+    done();
 });
 
-gulp.task('compileSass', () => {
-  return gulp.src('styles/app.scss')
-        .pipe(maps.init())
-        .pipe(sass())
-        .pipe(maps.write('./'))
-        .pipe(gulp.dest('css'));
-});
-
-gulp.task('compilePug', () => {
-  return gulp.src('views/index.pug')
-        .pipe(pug())
-        .pipe(gulp.dest('dist'));
-});
-
-gulp.task('watch', () =>{
-  gulp.watch('styles/**/*.scss', ['compileSass']);
-  gulp.watch('views/**', ['compilePug']);
-  gulp.watch('styles/**', ['minifyScripts']);
-});
-
-gulp.task('build', ['minifyScripts', 'compileSass'], () => {
-  return gulp.src(['css/app.css*', 'js/app.js', 'images/**', 'views/index.pug'], { base: './'})
+gulp.task('build', ['minifyScripts', 'compileSass', 'compilePug'], () => {
+  return gulp.src(['images/**'], { base: './'})
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', () => {
-  del(['dist', 'css/**', 'js/app*.js*']);
+  del(['dist']);
 });
 
-gulp.task('default', ['clean'], () => {
-  gulp.start('build');
+gulp.task('clean-build', () => {
+  runSequence('clean', 'build');
+});
+
+gulp.task('serve', ['clean-build'], () => {
+    browserSync.init({
+        server: "./dist"
+    });
+    gulp.watch('styles/**/*.scss', ['compileSass']);
+    gulp.watch('views/**', ['compilePug']);
+    gulp.watch('js/*', ['watch::js']);
+});
+
+gulp.task('default', () => {
+  gulp.start('serve');
 });
