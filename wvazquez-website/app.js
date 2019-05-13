27a39890c782c3bb5a4e5470cb8d44e17f6a73a5
wilmer vazquez <vazquezwilmer@gmail.com>
2019-05-13 15:33:10 -0400

    Changes handlebars to express-handebars and fixes livereload bug for handlebars partials.

--- wvazquez-website/app.js
+++ wvazquez-website/app.js
@@ -4,22 +4,36 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-const hbs = require('hbs');
+const hbs = require('express-handlebars');
+const livereloadMiddleware = require("connect-livereload");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine( 'hbs', hbs( { 
+  extname: 'hbs', 
+  defaultLayout: 'layout', 
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+} ) );
 app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials');
+
+// live reload setup
+var livereload = require('livereload').createServer({
+  exts: ['js','scss', 'hbs']
+});
+livereload.watch(path.join(__dirname));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(livereloadMiddleware());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -47,10 +61,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var livereload = require('livereload').createServer({
-  exts: ['js','scss', 'hbs']
-});
-livereload.watch(path.join(__dirname, 'views'));
-livereload.watch(path.join(__dirname, 'public'));
+
 
 module.exports = app;
