24965eaefb1d67dd265bed5afa6ea371f32d3fec
wilmer vazquez <vazquezwilmer@gmail.com>
2019-05-31 23:30:47 -0400

    Removes svg icons that were causing layout resizing bug.

--- wvazquez-website/app.js
+++ wvazquez-website/app.js
@@ -5,10 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 const hbs = require('express-handlebars');
-const livereloadMiddleware = require("connect-livereload");
+// const livereloadMiddleware = require("connect-livereload");
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -24,16 +24,16 @@ app.engine( 'hbs', hbs( {
 app.set('view engine', 'hbs');
 
 // live reload setup
-var livereload = require('livereload').createServer({
-  exts: ['js','scss', 'hbs']
-});
-livereload.watch(path.join(__dirname));
+// var livereload = require('livereload').createServer({
+//   exts: ['js','scss', 'hbs']
+// });
+// livereload.watch(path.join(__dirname));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(livereloadMiddleware());
+// app.use(livereloadMiddleware());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -43,23 +43,23 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 
