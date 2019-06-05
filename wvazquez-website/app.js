6bfbadeaec34ea2e9d55077b593e25de2b35c9a2
wvazquez <vazquezwilmer@gmail.com>
2019-06-05 02:55:05 -0400

    Creates a central config file and centralizes navigation menu items.

--- wvazquez-website/app.js
+++ wvazquez-website/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 const hbs = require('express-handlebars');
-// const livereloadMiddleware = require("connect-livereload");
+const livereloadMiddleware = require("connect-livereload");
+
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -24,16 +25,16 @@ app.engine( 'hbs', hbs( {
 app.set('view engine', 'hbs');
 
 // live reload setup
-// var livereload = require('livereload').createServer({
-//   exts: ['js','scss', 'hbs']
-// });
-// livereload.watch(path.join(__dirname));
+var livereload = require('livereload').createServer({
+  exts: ['js','scss', 'hbs']
+});
+livereload.watch(path.join(__dirname));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(livereloadMiddleware());
+app.use(livereloadMiddleware());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -50,7 +51,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// // error handler
+// error handler
 // app.use(function(err, req, res, next) {
 //   // set locals, only providing error in development
 //   res.locals.message = err.message;
---
+++ wvazquez-website/config/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    nav : [
+        {
+            title: 'About Me',
+            url: '/',
+        },
+        {
+            title: 'Work',
+            url: '/',
+        },
+        {
+            title: 'Clients',
+            url: '/',
+        },
+        {
+            title: 'Contact',
+            url: '/',
+        },
+    ]
+};
