835cd044ea410356a7f84ea9e16f0640dd65a2d8
wvazquez <vazquezwilmer@gmail.com>
2019-08-07 13:08:04 -0400

    Adds js to validate form.

--- wvazquez-website/public/javascript/functions.js
+++ wvazquez-website/public/javascript/functions.js
@@ -19,6 +19,7 @@ $( document ).ready(function() {
 
     
     smoothScroll();
+    form();
 
 });
 
@@ -58,4 +59,58 @@ function smoothScroll () {
 	        }, duration);
 	    }
 	});
+}
+
+
+
+function form() {
+
+    var input = $('.validate-input .input');
+
+    $('.validate-form').on('submit',function(e){
+        var check = true;
+        e.preventDefault();
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
 }
