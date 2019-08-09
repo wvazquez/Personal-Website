38025e0d1bd6a4a36fee4e9bd33e91a8be6956fd
wilmer vazquez <vazquezwilmer@gmail.com>
2019-08-09 12:01:41 -0400

    Moves all new projects files to the root.

--- app.js
+++ app.js
@@ -1,14 +1,68 @@
-const express = require('express');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var sassMiddleware = require('node-sass-middleware');
+const hbs = require('express-handlebars');
+const livereloadMiddleware = require("connect-livereload");
+var bodyParser = require('body-parser');
 
-app.set('view engine', 'pug');
 
-app.use(express.static('./dist'));
+var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
-app.get('/', (request, response)=> {
-  response.render('index');
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine( 'hbs', hbs( { 
+  extname: 'hbs', 
+  defaultLayout: 'layout', 
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/'
+} ) );
+app.set('view engine', 'hbs');
+
+// live reload setup
+var livereload = require('livereload').createServer({
+  exts: ['js','scss', 'hbs']
 });
+livereload.watch(path.join(__dirname));
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
-})
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(livereloadMiddleware());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+
+
+module.exports = app;
