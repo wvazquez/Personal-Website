a573db52e59580bf9aa0e4199502b2de5afc1a26
wvazquez <vazquezwilmer@gmail.com>
2019-08-21 13:38:28 -0400

    Adds hambuger menu with partial functionality.

--- public/javascript/functions.js
+++ public/javascript/functions.js
@@ -3,6 +3,7 @@ import { getProject, returnToProjectOverview } from "./projects.js";
 import { form } from "./emailSubmit.js";
 import { videoSetup } from "./video.js";
 import { modal } from './modal.js';
+import {toggle } from './toggleNavigation.js';
 
 $( document ).ready(function() {
 
@@ -23,5 +24,7 @@ $( document ).ready(function() {
     $(window).on('resize', videoSetup);
 
     //handles contact modal on 404 page.
-    $('#notfound .contact-btn').on('click', modal)
+    $('#notfound .contact-btn').on('click', modal);
+
+    $('.nav-toggler').on('click', toggle);
 });
---
+++ public/javascript/toggleNavigation.js
@@ -0,0 +1,13 @@
+
+const menu    = $('nav');
+const toggler = $('.nav-toggler');
+
+/*
+ * Toggles on and off the 'active' class on the menu
+ * and the toggler button.
+ */
+
+export function toggle(){
+    toggler.toggleClass('active');
+    menu.toggleClass('active');
+}
